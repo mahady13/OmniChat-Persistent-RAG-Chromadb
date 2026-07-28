0fb6a0fd1344f9893853322b7d41842993163ad3
mahady13 <mohiuddinmahady@gmail.com>
2026-07-28 21:45:34 +0600

    added models

--- main.py
+++ main.py
@@ -9,3 +9,17 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+load_dotenv()
+available_models={
+    "Ling 3 Flash": "inclusionai/ling-3.0-flash:free",
+    "Google Gemma 4-26b-a4b": "google/gemma-4-26b-a4b-it:free",
+    "Nvidia Nemotron 3 Ultra": "nvidia/nemotron-3-ultra-550b-a55b:free",
+    "Nvidia Nano Omni": "nvidia/nemotron-3-nano-omni-30b-a3b-reasoning:free",
+    "Nemotron 3 Super": "nvidia/nemotron-3-super-120b-a12b:free",
+    "Cohere: North Mini Code": "cohere/north-mini-code:free",
+    "PoolSide Laguna S2.1": "poolside/laguna-s-2.1:free",
+    "PoolSide Laguna XS2.1": "poolside/laguna-xs-2.1:free",
+    "OpenAI: gpt-oss-20b": "openai/gpt-oss-20b:free",
+    "Auto Free Router": "openrouter/free",
+}
